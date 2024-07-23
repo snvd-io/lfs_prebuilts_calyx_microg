1bfab5a8b53ac72fc462800d259973c223700ed4
Chirayu Desai <chirayudesai1@gmail.com>
2024-07-23 11:12:41 -0400

    Import files from previous repo, tag 'pre-lfs-20240722'
    Change-Id: I7e080f326ceaeba6e6a995d7d38f531175714db3

---
+++ Android.mk
@@ -0,0 +1,49 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GmsCore
+LOCAL_SRC_FILES := GmsCore/play-services-core-mapbox-default-release.apk
+LOCAL_CERTIFICATE := $(LOCAL_PATH)/certs/microg
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRODUCT_MODULE := true
+LOCAL_REQUIRED_MODULES := privapp-permissions-microg.xml whitelist-microg.xml
+LOCAL_USES_LIBRARIES := com.android.location.provider
+LOCAL_OPTIONAL_USES_LIBRARIES := org.apache.http.legacy androidx.window.extensions androidx.window.sidecar
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := FakeStore
+LOCAL_SRC_FILES := GmsCore/vending-app-default-release-unsigned.apk
+LOCAL_CERTIFICATE := $(LOCAL_PATH)/certs/microg
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRODUCT_MODULE := true
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GsfProxy
+LOCAL_SRC_FILES := GsfProxy/services-framework-proxy-release-unsigned.apk
+LOCAL_CERTIFICATE := $(LOCAL_PATH)/certs/microg
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-microg.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := whitelist-microg.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/sysconfig
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
